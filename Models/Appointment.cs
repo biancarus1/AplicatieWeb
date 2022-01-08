b67cbc16e739b7414b6bfc25ed6322944014d84b
BIANCA-MARIA RUS <bianca.maria.rus1@stud.ubbcluj.ro>
2022-01-08 22:00:59 +0200

    modificari

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -13,7 +13,6 @@ namespace AplicatieWeb.Models
         [Required, StringLength(150, MinimumLength = 3)]
         [Display(Name = "Nume Client")]
         public string Nume { get; set; }
-        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume', Required, StringLength(50, MinimumLength = 3")]
         [Display(Name = "Prenume Client")]
         public string Prenume { get; set; }
         public string Telefon { get; set; }
